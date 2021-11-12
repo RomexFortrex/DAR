b16a029e2d324b0c881aaf53ca8b402dd3cdd8b9
RomexFortrex <83926285+RomexFortrex@users.noreply.github.com>
2021-11-12 22:44:06 +0100

    Update ISIGame.java

--- src/tp2/ISIGame.java
+++ src/tp2/ISIGame.java
@@ -12,13 +12,15 @@ public class ISIGame {
 
     public static void main(String[] args) {
         System.out.println("Enter a word");
-
         Scanner scanner = new Scanner(System.in);
         String line = scanner.nextLine();
-
-        // now you have to check if the word contains 'isi' or not
-        // if not print 'ok'
-        // if yes throw the exception 'ISIException'
-        // and when you catch it print 'life is not isi'
+        try {
+            if (line.toLowerCase().contains("isi"))
+            System.out.println("Life is not isi");
+            else
+                throw new ISIException(line);
+        } catch(ISIException e){
+            System.out.printf("ok");
+        }
     }
 }
