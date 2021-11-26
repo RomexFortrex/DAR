7441ee887f25259337d3da7d8c6a9a10a9f0d1eb
RomexFortrex <83926285+RomexFortrex@users.noreply.github.com>
2021-11-26 20:25:12 +0100

    Update MyThread.java

--- src/tp3/ex2/MyThread.java
+++ src/tp3/ex2/MyThread.java
@@ -9,8 +9,7 @@ public class MyThread extends Thread{
 
     @Override
     public void run() {
-        //todo: implement the methods and uncomment the lines below
-        //StaticCounter.increment();
-        //counter.count();
+        StaticCounter.increment();
+        synchronized (counter){counter.count();};
     }
 }
