fbb906f056d01a46e8705a2dfdadf46024770b5f
RomexFortrex <83926285+RomexFortrex@users.noreply.github.com>
2021-11-26 20:26:35 +0100

    Update StaticCounter.java

--- src/tp3/ex2/StaticCounter.java
+++ src/tp3/ex2/StaticCounter.java
@@ -1,10 +1,11 @@
 package tp3.ex2;
 
 public class StaticCounter {
+    
     private static int counter = 0;
-
-    // todo : implement me
-    public static void increment(){
-        //increment counter and print it
+    
+    public synchronized static void increment() {
+        counter++;
+        System.out.println("Counter : "+counter+"\n");
     }
 }
