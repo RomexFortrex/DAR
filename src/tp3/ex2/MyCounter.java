d08e131c25190a259593e9831fcea1125459829f
RomexFortrex <83926285+RomexFortrex@users.noreply.github.com>
2021-11-26 20:24:12 +0100

    Update MyCounter.java

--- src/tp3/ex2/MyCounter.java
+++ src/tp3/ex2/MyCounter.java
@@ -1,9 +1,10 @@
 package tp3.ex2;
 
 public class MyCounter {
-
-    //todo : implement me
+    
     public void count(){
-        //print the first 500 numbers and then print 'finish !!'
+        for(int i=1;i<=500;i++)
+            System.out.println(i+"\n");
+        System.out.println("Finish !");
     }
 }
