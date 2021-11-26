2c94fa349c8cf1c9db76e47147c202f179449dbf
RomexFortrex <83926285+RomexFortrex@users.noreply.github.com>
2021-11-26 20:36:04 +0100

    Update Vendor.java

--- src/tp3/ex3/Vendor.java
+++ src/tp3/ex3/Vendor.java
@@ -8,13 +8,13 @@ public class Vendor extends Thread{
         super(s);
         this.isiSandwich = isiSandwich;
     }
-
     @Override
     public void run() {
         try {
             sleep(2000);
                 System.out.println("Vendor "+this.getName()+" said 'isi sandwich is ready'");
-                //todo : prepare a sandwich, set it as ready and notify a student
+                isiSandwich.setReady(true);
+                synchronized (isiSandwich){isiSandwich.notify();}
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
